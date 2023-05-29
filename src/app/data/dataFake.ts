32c9718a59dbf065f8d715fe5ad4150ccfb32dfb
Claitonjc <claitoncharlene@outlook.com>
2023-05-29 04:43:33 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,37 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Novo jogo GTA 5 é a maior febre do momento!",
+    "desciption": "O jogo Grand Theft Auto (GTA) V foi desenvolvido pela Rockstar North e publicado pela Rockstar Games. É o sétimo título principal da série GTA e foi lançado originalmente em 17 de setembro de 2013. O game se passa no estado ficcional de San Andreas, baseado na Califórnia do Sul, nos EUA. Traz a história de campanha simultânea de três criminosos o ladrão de bancos aposentado Michael Mike De Santa, o gângster de rua Franklin Clinton e o traficante de armas psicopata Trevor Philips. Seu mundo aberto permite que os jogadores naveguem livremente pelas áreas rurais e urbanas de San Andreas.",
+
+    "photoCover": "https://viciados.net/wp-content/uploads/2022/12/GTA-5-Grand-Theft-Auto-V-Ray-Tracing-Rockstar-Games-RTX-ON-2023.webp"
+  },
+  {
+    "id":"2",
+    "title": "Grand Theft Auto San Andreas",
+    "desciption": "Grand Theft Auto: San Andreas é um jogo eletrônico de ação-aventura desenvolvido pela Rockstar North e publicado pela Rockstar Games. É o quinto título principal da série Grand Theft Auto e foi lançado em outubro de 2004 para PlayStation 2 e em junho de 2005 para Xbox e Microsoft Windows. Versões para plataformas móveis foram lançadas inicialmente em 2013, enquanto que uma versão aprimorada, sob o subtítulo The Definitive Edition, foi lançada em 2021. O jogo se passa no estado ficcional de San Andreas, com a história seguindo Carl CJ Johnson em sua luta para lidar com guerras de gangues, confrontos com policiais e as relações com sua família e amigos. O mundo aberto permite que os jogadores naveguem livremente pelas áreas rurais e urbanas de San Andreas.",
+    "photoCover": "https://files.tecnoblog.net/wp-content/uploads/2019/07/gta-san-andreas-cover.jpg"
+
+  },
+  {
+    "id":"3",
+    "title": "Resident Evil 4",
+    "desciption": "A história segue o agente especial Leon S. Kennedy, que é enviado em uma missão para resgatar a filha do presidente dos Estados Unidos, Ashley Graham, que foi sequestrada por um culto religioso em uma parte rural da Espanha. Leon luta contra hordas de inimigos infectados por um parasita controlador de mentes e se reúne com a espiã Ada Wong. Afastando-se dos ângulos de câmera fixa e do estilo de jogo mais lento dos títulos anteriores, Resident Evil 4 apresenta um sistema de câmera dinâmico e jogabilidade orientada para a ação.",
+    "photoCover": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/pt_BR/games/switch/r/resident-evil-4-switch/hero"
+
+  },
+  {
+    "id":"4",
+    "title": "Max Payne 3",
+    "desciption": "Max Payne 3 é um jogo de tiro na terceira pessoa no qual o jogador assume o papel de seu personagem principal, Max Payne. Max Payne 3 contará com o zoom sobre o ombro e mecânica de cobertura. No entanto, estes só vão servir como enfeite do clássico correr e disparar do jogo. Em Max Payne 3 regressará também o bullet-time nas sequências de ação, em que a franquia é bastante notável. Em bullet-time, será possível ver todas as balas fazerem um buraco nos seus inimigos.",
+    "photoCover": "https://s2.glbimg.com/VGyIDNybsQIpMCNUTEuy3Qe_YHw=/0x0:695x434/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/3/2/AWxrZ4SRaIzRq42l9Cig/2016-04-25-max-payne-3-steam-page-pagina-jogo-game-comprar-techtudo-cover-capa-e1461380142406.jpg"
+
+  },
+  {
+    "id":"5",
+    "title": "Dead Space",
+    "desciption": "O jogador assume o papel de Isaac Clarke (referência aos escritores de ficção científica Isaac Asimov e Arthur C. Clarke), um engenheiro que deve sobreviver em uma nave de mineração infestada com monstros hostis conhecidos como Necromorfos, que são corpos humanos transformados e reanimados por um organismo alienígena. A visão do jogo é perspectiva de terceira pessoa sobre o ombro. A interface de Dead Space não apresenta o HUD tradicional. Ao invés disso, toda a informação é passada para o jogador através de projeções holográficas;",
+    "photoCover": "https://wp-uploads.qualbert.com/2023/01/content-1-1170x700.png"
+
+  },
+]
